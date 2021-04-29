4629591707f8043b4a9edeaec8af0c9b4f2ed5cc
Adam Hodgkinson <hodgkinson.adam@gmail.com>
2021-04-29 12:01:09 +0100

    should change working dir now

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,33 @@
 import 'source-map-support/register';
 
 import express, { Application } from 'express';
-import { ChildProcess, spawn } from 'child_process';
+import { spawn } from 'child_process';
 
 import { logger } from '@noodlewrecker7/logger';
 import Logger = logger.Logger;
-import * as Process from 'process';
 
 Logger.setLevel(Logger.Levels.TRACE);
 
-const PORT: number = 1234;
+const PORT = 1234;
 
 const app: Application = express();
 
-var pythonLog: String = '';
+let pythonLog = '';
 
 app.get('/log', (req, res) => {
   res.send(pythonLog);
 });
 
 app.listen(PORT, () => {
-  console.log('listening on port: ' + PORT);
-  const python = spawn('python', ['bot.py']);
+  Logger.info('listening on port: ' + PORT);
+  const python = spawn('python3', ['bot.py'], { cwd: 'innkeeper' });
 
   python.stdout.on('data', function (data) {
-    console.log(data.toString());
     Logger.debug(data.toString());
     pythonLog += data.toString();
   });
   python.stderr.on('data', (data) => {
-    console.log(data.toString());
     Logger.error(data.toString());
     pythonLog += data.toString();
   });
 });
-
-console.log('hello');
